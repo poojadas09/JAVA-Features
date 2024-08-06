24ad25541b9a312275c5b48fcfa7a17ac56ace10
poojadas09 <poojadas053@gmail.com>
2024-08-06 22:36:13 +0530

    Remove Duplicate from String using hashing

---
+++ src/RemoveDuplicatefromString.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class RemoveDuplicatefromString {
+    public static void main(String[] args) {
+        String inputString = "I love coding world";
+        StringBuilder str = new StringBuilder();
+        HashSet<Character> hashset = new HashSet<>();
+        for (char ch : inputString.toCharArray()) {
+            if (hashset.add(ch)) {
+                str.append(ch);
+            }
+        }
+        System.out.println(inputString);
+        System.out.println(str.toString());
+    }
+}
