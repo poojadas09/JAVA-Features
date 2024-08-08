37ef6c94bdd09cd1710879e51e0cb08618a71125
poojadas09 <poojadas053@gmail.com>
2024-08-08 23:44:18 +0530

    removed using hashset , instead used getOrDefault method

--- src/FindOccuranceOfCharacterinString.java
+++ src/FindOccuranceOfCharacterinString.java
@@ -1,17 +1,19 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FindOccuranceOfCharacterinString {
     public static void main(String[] args) {
         String str = "I love Coding World";
-        HashSet<Character> hashSet = new HashSet<>();
+        // create hashmap to store character occurrences
         HashMap<Character, Integer> hashMap = new HashMap<>();
+        // convert the string to character array to iterate
         for (char ch : str.toCharArray()) {
+            // convert each character to lowercase
             ch = Character.toLowerCase(ch);
-            if (hashSet.add(ch)) {
-                hashMap.put(ch, 1);
-            } else {
-                int count = hashMap.get(ch);
-                hashMap.put(ch, ++count);
+            // check if the character is a letter
+            if (Character.isLetter(ch)) {
+                // add or update the count in the hashmap
+                hashMap.put(ch, hashMap.getOrDefault(ch, 0) + 1);
             }
         }
         System.out.println(hashMap);
