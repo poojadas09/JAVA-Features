c0eb3a8b4afa697dac7173c6d4b46f0181d4d598
Pooja <poojadas053@gmail.com>
2024-08-28 23:07:36 +0530

    Print First Unique Character in a String

--- src/PrintFirstUniqueCharacterInaString.java
+++ src/PrintFirstUniqueCharacterInaString.java
@@ -1,4 +1,5 @@
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -6,11 +7,11 @@ public class PrintFirstUniqueCharacterInaString {
     public static void main(String[] args) {
         String input = "gyfdsdgsfhff";
         Character result =  input.chars()           // IntStream
-                .mapToObj(i -> Character.toLowerCase(Character.valueOf((char) i)))  // convert to lowercase & then to Character object Stream
+                .mapToObj(i -> Character.toLowerCase((char) i))  // convert to lowercase & then to Character object Stream
                 .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) // store in a LinkedHashMap with the count
                 .entrySet().stream()                       // EntrySet stream
                 .filter(entry -> entry.getValue() == 1)   // extracts characters with a count of 1
-                .map(entry -> entry.getKey())              // get the keys of EntrySet
+                .map(Map.Entry::getKey)              // get the keys of EntrySet
                 .findFirst().get();
         System.out.println(result);
     }
